57f9a5e9883c0742e4f6e277c31e940777283d2d
LukeHiwalker4 <55761783+LukeHiwalker4@users.noreply.github.com>
2021-01-27 14:10:57 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -2,11 +2,8 @@ document.cookie = "Luke#Password#true";
 
 var parts = document.cookie.split("#");
 
-alert(parts[0]);
-alert(parts[1]);
-alert(parts[2]);
-
 if(parts[2] == "true")
 {
-    alert("logged in!"); 
+    document.getElementById('name').value = parts[0];
+    document.getElementById('password').value = parts[1];
 }
