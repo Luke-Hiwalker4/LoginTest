e63ab4488955042d7fb390a1d8ee56c7575aa659
LukeHiwalker4 <55761783+LukeHiwalker4@users.noreply.github.com>
2021-01-27 14:45:38 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,32 +1,54 @@
 var parts = document.cookie.split("#");
 
-if(parts[2] == "true")
-{
-document.getElementById('enteredusername').innerHTML = parts[0];
+if (parts[2] == "true") {
+    document.getElementById('enteredusername').innerHTML = parts[0];
 }
 
-function onRegister()
-{
-document.cookie = document.getElementById('name').value
-+ "#" + document.getElementById('password').value
-+ "#" + "true";
-alert("Successfully registered!");
+function onRegister() {
+    document.cookie = document.getElementById('name').value
+        + "#" + document.getElementById('password').value
+        + "#" + "true";
+    alert("Successfully registered!");
 }
 
-function onCheck()
-{
-var enteredun = document.getElementById('name').value;
-var enteredpw = document.getElementById('password').value;
-var username = document.cookie.split("#")[0];
-var password = document.cookie.split("#")[1];
+function onCheck() {
+    var enteredun = document.getElementById('name').value;
+    var enteredpw = document.getElementById('password').value;
+    var username = document.cookie.split("#")[0];
+    var password = document.cookie.split("#")[1];
 
-if(password != enteredpw || username != enteredun)
-{
-alert("Wrong username / password.");
+    if (password != enteredpw || username != enteredun) {
+        alert("Wrong username / password.");
+    }
+
+    else {
+        alert("Successfully logged in!");
+    }
+}
+var parts = document.cookie.split("#");
+
+if (parts[2] == "true") {
+    document.getElementById('enteredusername').innerHTML = parts[0];
 }
 
-else
-{
-alert("Successfully logged in!");
+function onRegister() {
+    document.cookie = document.getElementById('name').value
+        + "#" + document.getElementById('password').value
+        + "#" + "true";
+    alert("Successfully registered!");
 }
+
+function onCheck() {
+    var enteredun = document.getElementById('name').value;
+    var enteredpw = document.getElementById('password').value;
+    var username = document.cookie.split("#")[0];
+    var password = document.cookie.split("#")[1];
+
+    if (password != enteredpw || username != enteredun) {
+        alert("Wrong username / password.");
+    }
+
+    else {
+        alert("Successfully logged in!");
+    }
 }
