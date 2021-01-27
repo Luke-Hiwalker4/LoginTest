cb86d119ab3f1ac8c67bc4a7e857dd3deb334e40
LukeHiwalker4 <55761783+LukeHiwalker4@users.noreply.github.com>
2021-01-27 14:47:46 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -25,30 +25,3 @@ function onCheck() {
         alert("Successfully logged in!");
     }
 }
-var parts = document.cookie.split("#");
-
-if (parts[2] == "true") {
-    document.getElementById('enteredusername').innerHTML = parts[0];
-}
-
-function onRegister() {
-    document.cookie = document.getElementById('name').value
-        + "#" + document.getElementById('password').value
-        + "#" + "true";
-    alert("Successfully registered!");
-}
-
-function onCheck() {
-    var enteredun = document.getElementById('name').value;
-    var enteredpw = document.getElementById('password').value;
-    var username = document.cookie.split("#")[0];
-    var password = document.cookie.split("#")[1];
-
-    if (password != enteredpw || username != enteredun) {
-        alert("Wrong username / password.");
-    }
-
-    else {
-        alert("Successfully logged in!");
-    }
-}
